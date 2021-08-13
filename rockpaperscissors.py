f5274dd0f48f6699c4a8d754250436d799a4410c
kiraaathan <email.akshaisreekumar@gmail.com>
2021-08-13 08:35:21 -0700

    Add rules for the game

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,18 +1,23 @@
-# Game rockpaperscissors
+# Game rock paper scissors
 import random
 
-print("Rules for the game!")
-print("Press \n 0 for Rock \n 1 for paper \n 2 for scissor")
-print("Enter your hand : ")
+print("Rules for the game!\n")
+print(
+    "* Rock wins over scissors\n* Paper wins over Rocks\n* Scissors wins over Paper\n"
+)
+print("How to play the game!\n")
+print("Press \n 0 for Rock \n 1 for paper \n 2 for scissors")
+print("\nEnter your hand : ")
 user_hand = int(input())
 random_hand = int(random.randint(0, 2))
 print("Opponent played :")
 print(random_hand)
-if random_hand == user_hand:
-    print("Draw!")
-elif random_hand > user_hand:
-    print("You lose!")
-elif random_hand < user_hand:
-    print("You win!")
-else:
+if user_hand > 2:
     print("Invalid entry!")
+else:
+    if random_hand == user_hand:
+        print("Draw Match!")
+    elif random_hand > user_hand:
+        print("You lose!")
+    elif random_hand < user_hand:
+        print("You win!")
