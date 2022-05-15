ed3d98263da867d8bec7172a0870d325bd3e07f4
kiraaathan <email.akshaisreekumar@gmail.com>
2022-05-16 01:08:22 +0530

    Add sum of digit program

---
+++ sum_of_digits.py
@@ -0,0 +1,7 @@
+#Sum of digits
+number = input("Type a two digit number:")
+print("First digit is " + number[0] + " and " + "second digit is "+ number[1])
+first_digit =number[0]
+second_digit=number[1]
+sum_of_digits = int(first_digit) + int(second_digit)
+print(f'Sum of digits is {sum_of_digits}')
