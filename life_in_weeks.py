9d9387e012f97da9fead32a373f622fcee1fc3ce
kiraaathan <email.akshaisreekumar@gmail.com>
2022-06-14 23:54:03 +0530

    Add program life in weeks

---
+++ life_in_weeks.py
@@ -0,0 +1,7 @@
+#Life in weeks 
+age = input("Enter your current age :")
+age = int(age)
+remaining_days = (90 - age)*365
+remaining_weeks = (90 -age)*56
+remaining_months = (90 -age)*12
+print(f"You have {remaining_days} days ,{remaining_weeks} weeks & {remaining_months} months until you reach 90 years old!")
