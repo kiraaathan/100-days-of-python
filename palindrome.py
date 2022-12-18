14868b7f4ec04a613528d18ea7c786b0a71d3b64
kiraaathan <email.akshaisreekumar@gmail.com>
2022-12-18 13:57:16 +0530

    program to check palindrome number or not

---
+++ palindrome.py
@@ -0,0 +1,7 @@
+#Palindrome number 
+number = input("Type a number:")
+reversed_number = number [::-1]
+if number == reversed_number:
+    print(f'The {number} is palindrome!')
+else :    
+    print(f'The {number} is not 1palindrome!')
